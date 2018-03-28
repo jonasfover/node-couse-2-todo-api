b93774109e1442b3b75dee01f8e46e3e8922de35
Jonas Overgaard <jonasfovergaard@gmail.com>
2018-03-28 16:24:49 +0200

    Add GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,40 +1,51 @@
-const {ObjectID} = require('mongodb');
+const expect = require('expect');
+const request = require('supertest');
 
-const {mongoose} = require('./../server/db/mongoose');
-const {Todo} = require('./../server/models/todo');
-const {User} = require('./../server/models/user');
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
 
-// var id = '57bf38394b39c93d2a557e9811';
-//
-// if (!ObjectID.isValid(id)) {
-//   console.log('ID not valid');
-// }
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
 
-// Todo.find({
-//   _id: id
-// }).then((todos) => {
-//   console.log('Todos', todos);
-// });
-//
-// Todo.findOne({
-//   _id: id
-// }).then((todo) => {
-//   console.log('Todo', todo);
-// });
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
 
-// Todo.findById(id).then((todo) => {
-//   if (!todo) {
-//     return console.log('Id not found');
-//   }
-//   console.log('Todo By Id', todo);
-// }).catch((e) => console.log(e));
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
-User.findById('57bdb0fcdedf88540bfa2d66').then((user) => {
-  if (!user) {
-    return console.log('Unable to find user');
-  }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
 
-  console.log(JSON.stringify(user, undefined, 2));
-}, (e) => {
-  console.log(e);
+  it('should not create todo with invalid body data', (done) => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
 });
