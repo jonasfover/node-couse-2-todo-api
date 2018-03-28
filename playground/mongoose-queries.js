5684c141fdcff720f0e2b7f4eaa0a58629a0af17
Jonas Overgaard <jonasfovergaard@gmail.com>
2018-03-28 15:13:03 +0200

    add queries file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -29,7 +29,7 @@ const {User} = require('./../server/models/user');
 //   console.log('Todo By Id', todo);
 // }).catch((e) => console.log(e));
 
-User.findById('57bdb0fcdedf88540bfa2d66').then((user) => {
+User.findById('5ab95b664dd09b04a8f7d8c0').then((user) => {
   if (!user) {
     return console.log('Unable to find user');
   }
